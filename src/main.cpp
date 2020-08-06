9756d4a153c74433d3223b785401b08b1ccd562f
9H5G <jramacrae@gmail.com>
2020-08-06 19:29:07 +0100

    Up to 6 august

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@
 #include "pwm.h"
 #include "thermoStat.h"
 
-#define RESOLUTION 11
+#define RESOLUTION 12
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(TEMPSIG);
@@ -130,7 +130,6 @@ void setup(void)
 
   setPins();
 
-  pinMode(FAULTIN, INPUT);
   attachInterrupt(FAULTIN, isr, RISING);
 
   fileSystemCheck();
@@ -182,14 +181,16 @@ void setup(void)
 
   Serial.print("Device 0 Resolution: ");
   Serial.print(sensors.getResolution(insideThermometer), DEC);
-   Serial.println();
+  Serial.println();
   sensors.requestTemperatures(); // Send the command to get temperatures
   vTaskDelay(300);
   float tempC = -99;
   tempC = sensors.getTempC(insideThermometer);
   Serial.println(tempC);
-  
+
   setupPWM();
+
+  myreconnect();
 }
 
 void handleFault()
@@ -249,10 +250,12 @@ void resetFaultTimer()
 }
 long lastReconnectAttempt = 0;
 
-boolean reconnect() {
-  if (client.connect(Settings.Host, Settings.ControllerUser, Settings.ControllerPassword)) {
+boolean reconnect()
+{
+  if (client.connect(Settings.Host, Settings.ControllerUser, Settings.ControllerPassword))
+  {
     // Once connected, publish an announcement...
-    client.publish("outTopic","hello world");
+    client.publish("outTopic", "hello world");
     // ... and resubscribe
     //client.subscribe("inTopic");
   }
@@ -261,63 +264,90 @@ boolean reconnect() {
 // function to print the temperature for a device
 void printTemperature(DeviceAddress deviceAddress)
 {
- // sensors.setWaitForConversion(false); // makes it async
-  sensors.requestTemperatures();
- // sensors.setWaitForConversion(true);
- 
-  delay(750 / (1 << (12 - RESOLUTION)));
-  tempC = sensors.getTempCByIndex(0);
+  do
+  {
+    sensors.requestTemperatures();
+    vTaskDelay(750 / (1 << (12 - RESOLUTION)));
+    tempC = sensors.getTempCByIndex(0);
+  } while ((tempC < -10) || (tempC > 50));
 
   // get temperature
   //Serial.println();
-  Serial.print("Temperature: ");
-  Serial.println(tempC);
+  if (tempC > -100)
+  {
+    Serial.print("Temperature: ");
+    Serial.println(tempC);
 
-  strcpy(pTopic, "");
-  strcat(pTopic, Settings.Host);
-  strcat(pTopic, "/tempc");
-  snprintf(msg, 63, "%f", tempC);
- 
- if (!client.connected()) {
-long now = millis();
-    if (now - lastReconnectAttempt > 5000) {
-      lastReconnectAttempt = now;
-      // Attempt to reconnect
-      if (reconnect()) {
-        lastReconnectAttempt = 0;
+    strcpy(pTopic, "");
+    strcat(pTopic, Settings.Host);
+    strcat(pTopic, "/tempc");
+    snprintf(msg, 63, "%f", tempC);
+
+    myreconnect();
+
+    if (!client.connected())
+    {
+      long now = millis();
+      if (now - lastReconnectAttempt > 5000)
+      {
+        lastReconnectAttempt = now;
+        // Attempt to reconnect
+        if (reconnect())
+        {
+          lastReconnectAttempt = 0;
+        }
       }
     }
- }
-  if (client.connect(Settings.Host, Settings.ControllerUser, Settings.ControllerPassword))
-  {
-    client.publish(pTopic, msg);
+    if (client.connect(Settings.Host, Settings.ControllerUser, Settings.ControllerPassword))
+    {
+      client.publish(pTopic, msg);
+      /*
+      Serial.print(Settings.Host);
+      Serial.print(" : ");
+      Serial.print(pTopic);
+      Serial.print(" : ");
+      Serial.println(msg);
+      */
+    }
   }
-
-/*
-digitalWrite(BUZZER, HIGH);
-vTaskDelay(01000);
-digitalWrite(BUZZER, LOW);
-*/
 }
+
+void runpins()
+{
+  digitalWrite(BUZZER, LOW);
+  vTaskDelay(100);
+  digitalWrite(BUZZER, HIGH);
+  vTaskDelay(100);
+  digitalWrite(BUZZER, LOW);
+}
+
 /*
  * Main function. It will request the tempC from the sensors and display on Serial.
  */
 void loop(void)
 {
   ArduinoOTA.handle();
-   client.loop();
+  client.loop();
+
   if (thermoCounter > 0)
   {
     portENTER_CRITICAL(&timerMux);
     thermoCounter--;
     portEXIT_CRITICAL(&timerMux);
-    printTemperature(insideThermometer); // Use a simple function to print out the data
-    myreconnect();
+
+    do
+    {
+      printTemperature(insideThermometer); // Use a simple function to print out the data
+    } while ((tempC < -10) && (tempC > 50));
+
+    runled(compSpeed);
+
+    runpins();
   }
-    
+
   handleFault();
 
   resetFaultTimer();
 
-  runled(compSpeed);
+  boot = false;
 }
--- src/pwm.h
+++ src/pwm.h
@@ -4,7 +4,7 @@ void setupPWM()
 {
 
 // use first channel of 16 channels (started from zero)
-#define LEDC_CHANNEL_0 8
+#define LEDC_CHANNEL_0 0
 
 // use 13 bit precission for LEDC timer
 #define LEDC_TIMER_13_BIT 13
@@ -12,7 +12,7 @@ void setupPWM()
 // use 5000 Hz as a LEDC base frequency
 #define LEDC_BASE_FREQ 5000
 
-// fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
+    // fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
 
     ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
     ledcAttachPin(PWM, LEDC_CHANNEL_0);
@@ -25,23 +25,22 @@ void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255)
 
     // write duty to LEDC
     ledcWrite(channel, duty);
+    Serial.print(duty);
 }
 
 void runled(int level)
 {
-    ledcAnalogWrite(LEDC_CHANNEL_0, level);
-
     // change the compSpeed for next time through the loop:
     compSpeed = compSpeed + fadeAmount;
 
     // reverse the direction of the fading at the ends of the fade:
-    if (compSpeed <= 0 || compSpeed >= 255)
+    if (compSpeed < 0 || compSpeed >= 255)
     {
-        fadeAmount = -fadeAmount;
+        //fadeAmount = -fadeAmount;
+        compSpeed = 0;
     }
+    ledcAnalogWrite(LEDC_CHANNEL_0, level);
     // wait for 30 milliseconds to see the dimming effect
-
-    delay(30);
+    Serial.print(" : ");
+    Serial.println(level);
 }
-
-
